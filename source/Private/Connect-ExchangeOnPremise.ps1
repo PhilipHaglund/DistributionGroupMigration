17f72e780786a8b7a2849a874a1f98a22e2b2a71
Philip Haglund <philip@gonjer.com>
2022-03-28 21:12:44 +0200

    Remove EX-Onpremise SSO Connection

--- source/Private/Connect-ExchangeOnPremise.ps1
+++ source/Private/Connect-ExchangeOnPremise.ps1
@@ -14,19 +14,12 @@
         try {
             $CurrentWarningPreference = $WarningPreference
             $WarningPreference = 'SilentlyContinue'
-            $Session = New-PSSession -Name EXCH -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$($ExchangeServer)/PowerShell/" -Authentication Kerberos -AllowRedirection -ErrorAction Stop -WarningAction SilentlyContinue
+            $Session = New-PSSession -Name EXCH -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$($ExchangeServer)/PowerShell/" -Credential (Get-Credential -Message 'Exchange On-premise credentials') -Authentication Kerberos -AllowRedirection -ErrorAction Stop -WarningAction SilentlyContinue
             Import-Module (Import-PSSession -Session $Session -Prefix EXCH -ErrorAction Stop -WarningAction SilentlyContinue) -Prefix EXCH -Global -ErrorAction Stop -WarningAction SilentlyContinue
             return $true
         }
         catch {
-            try {
-                Write-PSFMessage -Level Warning -Message 'Unable to use current credentials when authenticating to Exchange On-premise. Provide credentials to try again.'
-                $Session = New-PSSession -Name EXCH -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$($ExchangeServer)/PowerShell/" -Credential (Get-Credential -Message 'Exchange On-premise credentials') -Authentication Kerberos -AllowRedirection -ErrorAction Stop -WarningAction SilentlyContinue
-                Import-Module (Import-PSSession -Session $Session -Prefix EXCH -ErrorAction Stop -WarningAction SilentlyContinue) -Prefix EXCH -Global -ErrorAction Stop -WarningAction SilentlyContinue
-            }
-            catch {
-                $PSCmdlet.ThrowTerminatingError($_)
-            }
+            $PSCmdlet.ThrowTerminatingError($_)
         }
         finally {
             $WarningPreference = $CurrentWarningPreference

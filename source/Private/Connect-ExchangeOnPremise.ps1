6ad2a6c7070ff70d4953a312b97da4ed4f690817
Philip Haglund <philip@gonjer.com>
2022-05-10 21:47:26 +0200

    Final fix.

--- source/Private/Connect-ExchangeOnPremise.ps1
+++ source/Private/Connect-ExchangeOnPremise.ps1
@@ -4,7 +4,21 @@
     param(
         # Specifies an Exchange On-premise server hosting the PowerShell endpoint.
         [Parameter()]
-        [string]$ExchangeServer
+        [string]$ExchangeServer,
+
+        [ValidateNotNull()]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential = [System.Management.Automation.PSCredential]::Empty,
+
+        # The CommandName parameter specifies the comma separated list of commands to import into the session. Use this parameter for applications or scripts that use a specific set of cmdlets. Reducing the number of cmdlets in the session helps improve performance and reduces the memory footprint of the application or script.
+        [Parameter()]
+        [ValidateNotNull()]
+        [string[]]$CommandName = @('Get-DistributionGroup', 'Get-Recipient', 'New-DistributionGroup', 'Set-DistributionGroup', 'Update-DistributionGroupMember', 'Get-AcceptedDomain'),
+
+        # Force reconnect to Exchange On-Premise
+        [Parameter()]
+        [switch]$Force
     )
 
     if (Get-Command -Name 'Get-EXCHMailbox' -ErrorAction SilentlyContinue) {
@@ -12,10 +26,37 @@
     }
     else {
         try {
+            if (-not $PSBoundParameters.ContainsKey('CommandName')) {
+                [string[]]$CommandName = @('Get-DistributionGroup', 'Get-Recipient', 'New-DistributionGroup', 'Set-DistributionGroup', 'Update-DistributionGroupMember', 'Get-AcceptedDomain')
+            }
+            else {
+                [string[]]$CommandName += 'Get-AcceptedDomain'
+            }
+            if ($PSBoundParameters.ContainsKey('Force')) {
+                Remove-PSSession (Get-PSSession -ComputerName $ExchangeServer -ErrorAction SilentlyContinue -WarningAction SilentlyContinue) -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+                $Module = Get-Module -Name 'tmp*'
+                foreach ($Mod in $Module) {
+                    if ((Get-Module ($Mod.ExportedCommands.Keys)) -match 'Add-EXCHAcceptedDomain') {
+                        Remove-Module -Name $Mod.Name -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+                    }
+                }
+            }
             $CurrentWarningPreference = $WarningPreference
             $WarningPreference = 'SilentlyContinue'
-            $Session = New-PSSession -Name EXCH -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$($ExchangeServer)/PowerShell/" -Credential (Get-Credential -Message 'Exchange On-premise credentials') -Authentication Kerberos -AllowRedirection -ErrorAction Stop -WarningAction SilentlyContinue
-            Import-Module (Import-PSSession -Session $Session -Prefix EXCH -ErrorAction Stop -WarningAction SilentlyContinue) -Prefix EXCH -Global -ErrorAction Stop -WarningAction SilentlyContinue
+            $SessionParams = @{
+                Name = 'EXCH'
+                ConfigurationName = 'Microsoft.Exchange'
+                ConnectionUri = "http://$($ExchangeServer)/PowerShell/?SerializationLevel=Full"
+                Authentication = 'Kerberos'
+                AllowRedirection = $true
+                ErrorAction = 'Stop'
+            }
+
+            if ($Credential -ne [System.Management.Automation.PSCredential]::Empty) {
+                $null = $SessionParams.Add('Credential', $Credential)
+            }
+            $Session = New-PSSession @SessionParams
+            $null = Import-Module (Import-PSSession -Session $Session -Prefix EXCH -CommandName $CommandName -ErrorAction Stop -WarningAction SilentlyContinue -AllowClobber) -Prefix EXCH -Global -ErrorAction Stop
             return $true
         }
         catch {
--- source/Private/Connect-O365EXO.ps1
+++ source/Private/Connect-O365EXO.ps1
@@ -12,16 +12,40 @@
         # The Prefix parameter specifies an alias to add to nouns in the names of older remote PowerShell cmdlets (cmdlet with nouns that don't already start with EXO). A valid value is a text string without spaces or special characters like underscrores, asterisks etc, and you can't use the value EXO (this prefix is reserved for PowerShell V2 module cmdlets).
         [Parameter()]
         [ValidatePattern('^(?!.*EXO).*$')]
-        [string]$Prefix
+        [string]$Prefix,
+
+        # Force reconnect to Exchange Online
+        [Parameter()]
+        [switch]$Force
     )
 
     try {
         $Module = 'ExchangeOnlineManagement'
-        $Cmdlet = 'Get-EXOMailbox'
         $TestModule = Get-Module -Name $Module -ErrorAction Stop
         if (-not $TestModule) {
-            Import-Module -Name $Module -ErrorAction Stop
+            try {
+                Import-Module -Name $Module -ErrorAction Stop
+            }
+            catch {
+                throw ('The module "{0}" was not found. Install guide: https://aka.ms/EXOPowerShell.' -f $Module)
+            }
         }
+    }
+    catch {
+        throw $_
+    }
+
+    try {
+        if ($PSBoundParameters.ContainsKey('Force')) {
+            $null = Disconnect-ExchangeOnline -Confirm:$false
+        }
+    }
+    catch {
+        Write-PSFMessage -Level Warning -Message ('Unable to Disconnect-ExchangeOnline - {0}' -f $_.Exception.Message)
+    }
+
+    try {
+        $Cmdlet = 'Get-EXOMailbox'
         if ((Get-Command -Name $Cmdlet -ErrorAction SilentlyContinue)) {
             $TestConnection = & $Cmdlet -Identity 'DiscoverySearchMailbox*' -ErrorAction Stop
             if ($TestConnection) {
@@ -43,6 +67,9 @@
             }
             if ($CommandName.Count -gt 0) {
                 $BoundParameters.Add('CommandName', $CommandName)
+            }
+            else {
+                $BoundParameters.Add('CommandName', @('Get-DistributionGroup', 'Get-Recipient', 'New-DistributionGroup', 'Set-DistributionGroup', 'Update-DistributionGroupMember', 'Get-AcceptedDomain'))
             }
             if ($Prefix.Length -gt 0) {
                 $BoundParameters.Add('Prefix', $Prefix)
